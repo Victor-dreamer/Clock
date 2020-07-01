fab6c15648283e3f7c96dbe270f6913bb064f18e
Victor-dreamer <1225966942@qq.com>
2020-07-01 17:59:10 +0800

    使用standard更新代码格式

--- src/views/Clock.vue
+++ src/views/Clock.vue
@@ -68,7 +68,7 @@
             type="button"
             value="-"
             class="digital-time-item"
-            @click="changeTimeByButton(1,1,0),afterChangeTime"
+            @click="changeTimeByButton(1,1,0)"
           />
         </div>
         <!-- <p class="time">{{ time }}</p> -->
@@ -79,215 +79,215 @@
 
 <script>
 export default {
-  name: "clock",
-  data() {
+  name: 'clock',
+  data () {
     return {
       // date: new Date(),
       hour: 0,
       minute: 0,
       second: 0,
-      timerID: "",
+      timerID: '',
       isMouseDown: 0,
       haveChanged: 0,
       pointerX: 0,
       pointerY: 0
-    };
+    }
   },
   computed: {
     // 根据时间值计算对应的角度
-    miaoDeg: function() {
-      return parseInt(this.second) * 6;
+    miaoDeg: function () {
+      return parseInt(this.second) * 6
     },
-    fenDeg: function() {
-      return parseInt(this.minute) * 6 + parseInt(this.second) * 0.1;
+    fenDeg: function () {
+      return parseInt(this.minute) * 6 + parseInt(this.second) * 0.1
     },
-    shiDeg: function() {
-      return parseInt(this.minute) * 0.5 + parseInt(this.hour) * 30;
+    shiDeg: function () {
+      return parseInt(this.minute) * 0.5 + parseInt(this.hour) * 30
     },
     // 补充0之后展示时间
-    showSecond: function() {
-      return this.zeroPadding(parseInt(this.second), 2);
+    showSecond: function () {
+      return this.zeroPadding(parseInt(this.second), 2)
     },
-    showMinute: function() {
-      return this.zeroPadding(parseInt(this.minute), 2);
+    showMinute: function () {
+      return this.zeroPadding(parseInt(this.minute), 2)
     },
-    showHour: function() {
-      return this.zeroPadding(parseInt(this.hour), 2);
+    showHour: function () {
+      return this.zeroPadding(parseInt(this.hour), 2)
     }
   },
 
   watch: {
     // 侦听时间的变化
-    minute: function(val) {
+    minute: function (val) {
       // console.log(val);
       if (val >= 60) {
-        this.minute = val - 60;
-        this.hour++;
+        this.minute = val - 60
+        this.hour++
       } else if (val < 0) {
-        this.minute = val + 60;
-        this.hour--;
+        this.minute = val + 60
+        this.hour--
       }
     },
-    second: function(val) {
+    second: function (val) {
       // console.log(val);
       if (val >= 60) {
-        this.second = val - 60;
-        this.minute++;
+        this.second = val - 60
+        this.minute++
       } else if (val < 0) {
-        this.second = val + 60;
-        this.minute--;
+        this.second = val + 60
+        this.minute--
       }
     },
-    hour: function(val) {
+    hour: function (val) {
       // console.log(val);
       if (val >= 24) {
-        this.hour = val - 24;
+        this.hour = val - 24
       } else if (val < 0) {
-        this.hour = val + 24;
+        this.hour = val + 24
       }
     }
   },
 
-  mounted() {
-    this.addMarks();
-    this.updateTime();
-    this.timerID = setInterval(this.updateTime, 1000);
+  mounted () {
+    this.addMarks()
+    this.updateTime()
+    this.timerID = setInterval(this.updateTime, 1000)
   },
   methods: {
     // 添加钟表刻度
-    addMarks() {
-      let mList = document.getElementById("markList");
-      let marks = "";
-      let sCss = "";
+    addMarks () {
+      let mList = document.getElementById('markList')
+      let marks = ''
+      let sCss = ''
       for (let i = 0; i < 60; i++) {
-        sCss = "transform: rotate(" + i * 6 + "deg);";
-        marks += "<li style='" + sCss + "'></li>";
+        sCss = 'transform: rotate(' + i * 6 + 'deg);'
+        marks += "<li style='" + sCss + "'></li>"
       }
-      mList.innerHTML = marks;
+      mList.innerHTML = marks
     },
 
     // 更新时间
-    updateTime() {
-      let cd = new Date();
-      this.hour = cd.getHours();
-      this.minute = cd.getMinutes();
-      this.second = cd.getSeconds();
+    updateTime () {
+      let cd = new Date()
+      this.hour = cd.getHours()
+      this.minute = cd.getMinutes()
+      this.second = cd.getSeconds()
     },
 
     // 修改时间
-    changeTime(p, time, tag) {
-      let s = this.second;
-      let m = this.minute;
-      let h = this.hour;
+    changeTime (p, time, tag) {
+      let s = this.second
+      let m = this.minute
+      let h = this.hour
       // 根据参数判断时间的更改
-      if (tag == 1) {
+      if (tag === 1) {
         switch (p) {
           case 1:
-            this.second = s + time;
+            this.second = s + time
             // this.afterChangeTime();
-            break;
+            break
           case 2:
-            this.minute = m + time;
+            this.minute = m + time
             // this.afterChangeTime();
-            break;
+            break
           case 3:
-            this.hour = h + time;
+            this.hour = h + time
             // this.afterChangeTime();
-            break;
+            break
         }
       }
-      if (tag == 0) {
+      if (tag === 0) {
         switch (p) {
           case 1:
-            this.second = s - time;
+            this.second = s - time
             // this.afterChangeTime();
-            break;
+            break
           case 2:
-            this.minute = m - time;
+            this.minute = m - time
             // this.afterChangeTime();
-            break;
+            break
           case 3:
-            this.hour = h - time;
+            this.hour = h - time
             // this.afterChangeTime();
-            break;
+            break
         }
       }
     },
 
     // 按钮修改时间
-    changeTimeByButton(p, time, tag) {
-      if (this.haveChanged == 0) {
-        clearInterval(this.timerID);
+    changeTimeByButton (p, time, tag) {
+      if (this.haveChanged === 0) {
+        clearInterval(this.timerID)
         this.timerID = setInterval(() => {
-          this.changeTime(1, 1, 1);
-        }, 1000);
+          this.changeTime(1, 1, 1)
+        }, 1000)
         // console.log("使用按钮改变过时间！")
-        this.haveChanged = 1;
+        this.haveChanged = 1
       }
-      this.changeTime(p, time, tag);
+      this.changeTime(p, time, tag)
       // this.haveChanged = 1;
     },
 
     // 补充0
-    zeroPadding(num, digit) {
-      let zero = "";
+    zeroPadding (num, digit) {
+      let zero = ''
       for (let i = 0; i < digit; i++) {
-        zero += "0";
+        zero += '0'
       }
-      return (zero + num).slice(-digit);
+      return (zero + num).slice(-digit)
     },
 
     // 获取鼠标在四象限上的位置角度
-    getAngle(px, py, mx, my) {
-      let x = Math.abs(px - mx);
-      let y = Math.abs(py - my);
-      let z = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
-      let cos = y / z;
-      let radina = Math.acos(cos); //用反三角函数求弧度
-      let angle = Math.floor(180 / (Math.PI / radina)); //将弧度转换成角度
+    getAngle (px, py, mx, my) {
+      let x = Math.abs(px - mx)
+      let y = Math.abs(py - my)
+      let z = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2))
+      let cos = y / z
+      let radina = Math.acos(cos) //  用反三角函数求弧度
+      let angle = Math.floor(180 / (Math.PI / radina)) // 将弧度转换成角度
       if (mx > px && my > py) {
-        //鼠标在第四象限
-        angle = 180 - angle;
+        //  鼠标在第四象限
+        angle = 180 - angle
       }
-      if (mx == px && my > py) {
-        //鼠标在y轴负方向上
-        angle = 180;
+      if (mx === px && my > py) {
+        //  鼠标在y轴负方向上
+        angle = 180
       }
-      if (mx > px && my == py) {
-        //鼠标在x轴正方向上
-        angle = 90;
+      if (mx > px && my === py) {
+        //  鼠标在x轴正方向上
+        angle = 90
       }
       if (mx < px && my > py) {
-        //鼠标在第三象限
-        angle = 180 + angle;
+        //  鼠标在第三象限
+        angle = 180 + angle
       }
-      if (mx < px && my == py) {
-        //鼠标在x轴负方向
-        angle = 270;
+      if (mx < px && my === py) {
+        //  鼠标在x轴负方向
+        angle = 270
       }
       if (mx < px && my < py) {
-        //鼠标在第二象限
-        angle = 360 - angle;
+        //  鼠标在第二象限
+        angle = 360 - angle
       }
-      return angle;
+      return angle
     },
 
     // 获取鼠标转动角度
-    getAngle2(px, py, ox, oy, nx, ny) {
+    getAngle2 (px, py, ox, oy, nx, ny) {
       // 第一条边的平方
       let n1 =
         Math.abs(ox - px) * Math.abs(ox - px) +
-        Math.abs(oy - py) * Math.abs(oy - py);
+        Math.abs(oy - py) * Math.abs(oy - py)
       let n2 =
         Math.abs(ox - px) * Math.abs(ox - px) +
-        Math.abs(oy - py) * Math.abs(oy - py);
+        Math.abs(oy - py) * Math.abs(oy - py)
       let n3 =
         Math.abs(ox - nx) * Math.abs(ox - nx) +
-        Math.abs(oy - ny) * Math.abs(oy - ny);
-      let cos = (n1 + n2 - n3) / (2 * Math.sqrt(n1) * Math.sqrt(n2));
-      let radina = Math.acos(cos); //用反三角函数求弧度
-      let angle = Math.floor(180 / (Math.PI / radina)); //将弧度转换成角度
-      return angle;
+        Math.abs(oy - ny) * Math.abs(oy - ny)
+      let cos = (n1 + n2 - n3) / (2 * Math.sqrt(n1) * Math.sqrt(n2))
+      let radina = Math.acos(cos) // 用反三角函数求弧度
+      let angle = Math.floor(180 / (Math.PI / radina)) // 将弧度转换成角度
+      return angle
     },
 
     /* 鼠标点击指针转动事件
@@ -321,104 +321,103 @@ export default {
     }, */
 
     // 鼠标按下事件
-    mouseDown(p) {
+    mouseDown (p) {
       // const this_ = this
-      this.isMouseDown = p;
-      clearInterval(this.timerID);
+      this.isMouseDown = p
+      clearInterval(this.timerID)
     },
 
-    //获取旋转角度并判断正负
-    pointerRotate(e) {
+    //  获取旋转角度并判断正负
+    pointerRotate (e) {
       // 获取钟表中心位置
-      let p = document.querySelector(".dial");
-      let x = p.offsetLeft + p.offsetWidth / 2;
-      let y = p.offsetTop + p.offsetHeight / 2;
-      let ang;
+      let p = document.querySelector('.dial')
+      let x = p.offsetLeft + p.offsetWidth / 2
+      let y = p.offsetTop + p.offsetHeight / 2
+      let ang
       // 判断按下
-      let md = this.isMouseDown;
-      if (md != 0) {
-        /* 
+      let md = this.isMouseDown
+      if (md !== 0) {
+        /*
           1.分离角度，并判断正负
           2.根据角度再修改时间，注意时的角度与分秒不同
         */
         // 旧坐标
-        var oldang = this.getAngle(x, y, this.pointerX, this.pointerY);
+        var oldang = this.getAngle(x, y, this.pointerX, this.pointerY)
         // 新坐标
-        var newang = this.getAngle(x, y, e.clientX, e.clientY);
+        var newang = this.getAngle(x, y, e.clientX, e.clientY)
         // 判断临界情况
         if (
-          clock.pointerY >= y &&
+          this.pointerY >= y &&
           e.clientY >= y &&
-          clock.pointerX >= x &&
+          this.pointerX >= x &&
           e.clientX < x
         ) {
           // 鼠标跨越0刻度，判断减少
           ang = this.getAngle2(
             x,
             y,
-            clock.pointerX,
-            clock.pointerY,
+            this.pointerX,
+            this.pointerY,
             e.clientX,
             e.clientY
-          );
-          ang = 0 - ang;
+          )
+          ang = 0 - ang
           // this.changeTime(md, ang / 6, 0);
           // console.log(ang);
           // console.log("减少"+Math.floor(ang / 6)+"秒");
         } else if (
-          clock.pointerY >= y &&
+          this.pointerY >= y &&
           e.clientY >= y &&
-          clock.pointerX < x &&
+          this.pointerX < x &&
           e.clientX >= x
         ) {
           ang = this.getAngle2(
             x,
             y,
-            clock.pointerX,
-            clock.pointerY,
+            this.pointerX,
+            this.pointerY,
             e.clientX,
             e.clientY
-          );
+          )
           // console.log(ang);
           // this.changeTime(md, ang / 6, 1);
           // console.log("增加"+Math.floor(ang / 6)+"秒");
         } else {
-          ang = newang - oldang;
+          ang = newang - oldang
           // console.log(ang);
         }
         if (ang > 0) {
-          if (md == 3) {
-            this.changeTime(md, ang / 30, 1);
-          }else {
-            this.changeTime(md, ang / 6, 1);
+          if (md === 3) {
+            this.changeTime(md, ang / 30, 1)
+          } else {
+            this.changeTime(md, ang / 6, 1)
           }
           // console.log("增加"+Math.floor(ang / 6)+"秒");
         }
         if (ang < 0) {
-          ang = 0 - ang;
-          if (md == 3) {
-            this.changeTime(md, ang / 30, 0);
-          }else {
-            this.changeTime(md, ang / 6, 0);
+          ang = 0 - ang
+          if (md === 3) {
+            this.changeTime(md, ang / 30, 0)
+          } else {
+            this.changeTime(md, ang / 6, 0)
           }
           // console.log("减少"+Math.floor(ang / 6)+"秒");
         }
       }
-      this.pointerX = e.clientX;
-      this.pointerY = e.clientY;
+      this.pointerX = e.clientX
+      this.pointerY = e.clientY
     },
 
     // 鼠标松开事件
-    stopChangePointer() {
+    stopChangePointer () {
       // window.removeEventListener("mousemove", this.changeAngle(1));
-      this.isMouseDown = 0;
-      this.timerID = setInterval(this.updateTime, 1000);
-      this.haveChanged = 0;
+      this.isMouseDown = 0
+      this.timerID = setInterval(this.updateTime, 1000)
+      this.haveChanged = 0
     }
   }
-};
+}
 </script>
-
 
 <style lang='scss'>
 ul {
